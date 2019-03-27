9a4aa793f79aae3f9b1d6c52e2ea68f0b35ff14e
Yves Njumbe <ynjumbe23@gmail.com>
2019-03-27 02:04:11 +0000

    Automatically backed up by Learn

---
+++ Practice2.js
@@ -0,0 +1,28 @@
+function sayHello() {
+  console.log('Hello!')
+}
+
+sayHello()
+
+function sayHellotoIsabel () {
+  console.log('Hello, Isabel!')
+}
+
+sayHellotoIsabel()
+
+function sayHellotoSofia() {
+  console.log('Hello, Sofia!')
+}
+
+sayHellotoSofia()
+
+function sayHellotoBrendan () {
+  console.log('Hello, Brendan!')
+}
+
+sayHellotoBrendan()
+function doSomething(thing) {
+  console.log(thing)
+}
+
+doSomething ('anything')
---
+++ Practice3.js
@@ -0,0 +1,11 @@
+function sayHelloTo(nickname, firstName) {
+  console.log(`${nickname}, ${firstName}!`)
+}
+
+sayHelloTo('Easy', 'Yves')
+
+function say(color, lastname) {
+  console.log(`The color ${color} does not suit Yves ${lastname}!`)
+}
+
+say('Blue', 'Njumbe')
